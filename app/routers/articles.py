88a2f64e79bdae55ee3f15e4945a5c61e3bf5454
Julian Santos <ingjuliansantos@gmail.com>
2020-10-18 11:51:16 -0500

    [ADD]💾 Added stats saving endpoint

---
+++ app/routers/articles.py
@@ -0,0 +1,29 @@
+from fastapi import Depends, APIRouter, HTTPException
+
+from sqlalchemy.orm import Session
+
+from .. import articles_crud, stats_crud, schemas
+from ..database import SessionLocal
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+router = APIRouter()
+
+
+@router.post("/articles/", response_model=schemas.Article)
+async def create_article(article: schemas.Article, db: Session = Depends(get_db)):
+    db_article = articles_crud.get_article_by_url(db, article_url=article.article_url)
+    if db_article:
+        raise HTTPException(status_code=400, detail="Article already registered")
+    return articles_crud.create_article(db=db, article=article)
+
+
+@router.post("/scraper_stats/", response_model=schemas.ScrapingStats)
+async def create_stats(stats: schemas.ScrapingStats, db: Session = Depends(get_db)):
+    return stats_crud.create_stats(db=db, stats=stats)
---
+++ app/stats_crud.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+
+def create_stats(db: Session, stats: schemas.ScrapingStats):
+    db_stats = models.ScrapingStats(response_count=stats.response_count,
+    start_time=stats.start_time,
+    finish_time=stats.finish_time,
+    memory_usage_max=stats.memory_usage_max,
+    total_articles_added=stats.total_articles_added,
+    scraping_date=stats.scraping_date)
+
+    db.add(db_stats)
+    db.commit()
+    db.refresh(db_stats)
+    return db_stats
